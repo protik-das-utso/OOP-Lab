c4809491c5ef944d98bbdc2fcf691f90d4b3e6ca
protik-das-utso <protikdas018830@gmail.com>
2025-01-26 23:25:06 +0600

    exercise solution is added in lab 3

--- lab 3/src/Main.java
+++ lab 3/src/Main.java
@@ -64,64 +64,64 @@ public class Main {
 
 //////////////////////////////////////// OR /////////////////////////////////
 
-import java.util.Date;
-import java.util.Scanner;
-
-class Customer {
-    String cusid;
-    String name;
-    String email;
-
-    Customer (String cusid, String name, String email) {
-        this.cusid = cusid;
-        this.name = name;
-        this.email = email;
-    }
-    public void display() {
-        System.out.println("Customer ID: " + cusid + "\nName: " + name + "\nEmail: " + email);
-    }
-}
-class Item {
-    int itemid;
-    String name;
-    int price;
-
-    Item (int itemid, String name, int price) {
-        this.itemid = itemid;
-        this.name = name;
-        this.price = price;
-    }
-    public void display() {
-        System.out.println("Item ID: " + itemid + "\nName: " + name + "\nPrice: " + price);
-    }
-}
-
-class Buy {
-    Date date;
-    int quantity;
-    Customer customer;
-    Item item;
-
-    Buy (Date date, int quantity, Customer customer, Item item) {
-        this.date = date;
-        this.quantity = quantity;
-        this.customer = customer;
-        this.item = item;
-    }
-    public void display() {
-        System.out.println("Date : " + date + "\nQuantity: " + quantity);
-        customer.display();
-        item.display();
-    }
-}
-
-public class Main {
-    public static void main(String[] args) {
-        Customer c = new Customer("1", "Protik", "protikdas018830@gmail.com");
-        Item i = new Item(101, "Java Tricks Book", 100);
-
-        Date currentDate = new Date();
-        Buy b = new Buy(currentDate,2, c, i);
-        b.display();
-    }
-}
+//import java.util.Date;
+//import java.util.Scanner;
+//
+//class Customer {
+//    String cusid;
+//    String name;
+//    String email;
+//
+//    Customer (String cusid, String name, String email) {
+//        this.cusid = cusid;
+//        this.name = name;
+//        this.email = email;
+//    }
+//    public void display() {
+//        System.out.println("Customer ID: " + cusid + "\nName: " + name + "\nEmail: " + email);
+//    }
+//}
+//class Item {
+//    int itemid;
+//    String name;
+//    int price;
+//
+//    Item (int itemid, String name, int price) {
+//        this.itemid = itemid;
+//        this.name = name;
+//        this.price = price;
+//    }
+//    public void display() {
+//        System.out.println("Item ID: " + itemid + "\nName: " + name + "\nPrice: " + price);
+//    }
+//}
+//
+//class Buy {
+//    Date date;
+//    int quantity;
+//    Customer customer;
+//    Item item;
+//
+//    Buy (Date date, int quantity, Customer customer, Item item) {
+//        this.date = date;
+//        this.quantity = quantity;
+//        this.customer = customer;
+//        this.item = item;
+//    }
+//    public void display() {
+//        System.out.println("Date : " + date + "\nQuantity: " + quantity);
+//        customer.display();
+//        item.display();
+//    }
+//}
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Customer c = new Customer("1", "Protik", "protikdas018830@gmail.com");
+//        Item i = new Item(101, "Java Tricks Book", 100);
+//
+//        Date currentDate = new Date();
+//        Buy b = new Buy(currentDate,2, c, i);
+//        b.display();
+//    }
+//}
